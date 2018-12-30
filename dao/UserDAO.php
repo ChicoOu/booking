faffcecc0e7113279c38e68feb5ea299bb06bf98
Chico Ou <qingsmy217@hotmail.com>
2018-12-30 21:43:32 +0800

    change to class

--- dao/UserDAO.php
+++ dao/UserDAO.php
@@ -1,45 +1,55 @@
 <?php
 require('../model/user.php');
 
-$servername= 'localhost';
-$username = 'root';
-$password = '123456';
-$dbname = 'booking';
-$conn = null;
+class UserDAO {
+    private $servername= 'localhost';
+    private $username = 'root';
+    private $password = '123456';
+    private $dbname = 'booking';
+    private $conn = null;
 
-function connect(){
-    try{
-        $conn = new PDO('mysql:host=$servername;dbname=$dbname', $username, $password);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    function __construct(){
+        $this->connect();
     }
-    catch(PDOException $e)
-    {
-        echo($e->getMessage());
+
+    function __destruct(){
         $conn = null;
     }
-}
 
-function getAllUsers(){
-    if( $conn == null ){
-        connect();
-    } 
-
-    if( $conn == null ){
-        return null;
+    function connect(){
+        try{
+            $conn = new PDO('mysql:host=$servername;dbname=$dbname', $username, $password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        catch(PDOException $e)
+        {
+            echo($e->getMessage());
+            $conn = null;
+        }
     }
 
-    $sql = "SELECT * FROM user"; 
-    $res = $pdo->query($sql);
-    $result = array();
-    $i = 0;
-    foreach($res as $row){
-        $obj = new User();
-        $obj->setUserName($row['username']);
-        $obj->setPassword($row['password']);
-        $result[$i] = $obj; 
-        $i++;
+    function getAllUsers(){
+        if( $conn == null ){
+            connect();
+        } 
+
+        if( $conn == null ){
+            return null;
+        }
+
+        $sql = "SELECT * FROM user"; 
+        $res = $pdo->query($sql);
+        $result = array();
+        $i = 0;
+        foreach($res as $row){
+            $obj = new User();
+            $obj->setUserName($row['username']);
+            $obj->setPassword($row['password']);
+            $result[$i] = $obj; 
+            $i++;
+        }
+        $conn = null;
+        return $result;
     }
-    $conn = null;
-    return $result;
 }
 ?>
--- index.php
+++ index.php
@@ -5,7 +5,8 @@
 echo("Hello!");
 
 require('./dao/UserDAO.php');
-$users = getAllUsers();
+$dao = new UserDAO();
+$users = $dao->getAllUsers();
 foreach($users as $user){
     echo($user->getUserName() + "<br/>" + $user->getPassword());
 }
