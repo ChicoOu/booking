076cbe60def547448a0e4b3fe1951ea5282b76c4
Chico Ou <qingsmy217@hotmail.com>
2018-12-31 20:08:34 +0800

    added log

--- common/requesthelper.php
+++ common/requesthelper.php
@@ -27,7 +27,9 @@ class RequestHelper{
     }
 
     public function isPost(){
-        return ($_SERVER['REQUEST_METHOD'] == 'POST' && checkurlHash($GLOBALS['verify'])
+        echo($GLOBALS['verify']);
+        error_log($GLOBALS['verify']);
+        return ($_SERVER['REQUEST_METHOD'] == 'POST' /*&& checkurlHash($GLOBALS['verify'])*/
 &&(empty($_SERVER['HTTP_REFERER']) || preg_replace("~https?:\/\/([^\:\/]+).*~i", "\\1", $_SERVER['HTTP_REFERER']) == preg_replace("~([^\:]+).*~", "\\1", $_SERVER['HTTP_HOST']))) ? true : false;
     }
 
