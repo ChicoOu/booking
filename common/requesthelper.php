e5c29fa4de17056bbced55621377f78f1f60fae2
Chico Ou <qingsmy217@hotmail.com>
2018-12-31 19:11:08 +0800

    request

---
+++ common/requesthelper.php
@@ -0,0 +1,43 @@
+<?php
+class RequestHelper{
+    private static $s_instance = null;
+
+    private function __construct(){
+        
+    }
+
+    private function __destruct(){
+        
+    }
+
+    private function __clone(){
+        
+    }
+
+    public static function getInstance(){
+        if( !(self::$s_instance instanceof self) ){
+            self::$s_instance = new RequestHelper();
+        }
+
+        return self::$s_instance;
+    }
+
+    public function isGet(){
+        return $_SERVER['REQUEST_METHOD'] == 'GET' ? true : false;
+    }
+
+    public function isPost(){
+        return ($_SERVER['REQUEST_METHOD'] == 'POST' && checkurlHash($GLOBALS['verify'])
+&&(empty($_SERVER['HTTP_REFERER']) || preg_replace("~https?:\/\/([^\:\/]+).*~i", "\\1", $_SERVER['HTTP_REFERER']) == preg_replace("~([^\:]+).*~", "\\1", $_SERVER['HTTP_HOST']))) ? true : false;
+    }
+
+    public function isAjax(){
+        if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && 
+            strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
+?>
--- index.php
+++ index.php
@@ -4,6 +4,19 @@
 <?php
 echo("Hello!");
 
+require('./common/requesthelper.php');
+$helper = RequestHelper::getInstance();
+if( $helper.isGet() ){
+    echo("It's a get method!");
+}
+else if( $helper.isPost() ){
+    echo("It's a post method!");
+}
+else{
+    echo("It's unknonw method!");
+}
+
+
 require('./dao/UserDAO.php');
 $dao = new UserDAO();
 $users = $dao->getAllUsers();
